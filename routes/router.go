efa045af4d53c9210dbdf70cad714bcf5a4107e3
Ishaan Shah <70190533+ishaan812@users.noreply.github.com>
2023-02-17 12:59:34 +0530

    routes

--- routes/router.go
+++ routes/router.go
@@ -22,6 +22,29 @@ func InitializeRouter() {
 	r.HandleFunc("/logout", controllers.Logout).Methods("GET")
 	r.HandleFunc("/refresh", controllers.Refresh).Methods("GET")
 
+	r.HandleFunc("/participant", controllers.CreateParticipant).Methods("POST", "OPTIONS")
+	r.HandleFunc("/getAllParticipants", controllers.GetAllParticipants).Methods("GET")
+	r.HandleFunc("/participant/{id}", controllers.GetParticipantByID).Methods("GET")
+	r.HandleFunc("/participant/{id}", controllers.UpdateParticipant).Methods("PUT")
+	r.HandleFunc("/participant/{id}", controllers.DeleteParticipant).Methods("DELETE")
+
+	r.HandleFunc("/hackathon", controllers.CreateHackathon).Methods("POST", "OPTIONS")
+	r.HandleFunc("/getAllHackathons", controllers.GetAllHackathons).Methods("GET")
+	r.HandleFunc("/hackathon/{id}", controllers.GetHackathonByID).Methods("GET")
+	r.HandleFunc("/hackathon/{id}", controllers.UpdateHackathon).Methods("PUT")
+	r.HandleFunc("/hackathon/{id}", controllers.DeleteHackathon).Methods("DELETE")
+
+	r.HandleFunc("/organizer", controllers.CreateOrganizer).Methods("POST", "OPTIONS")
+	r.HandleFunc("/getAllOrganizers", controllers.GetAllOrganizers).Methods("GET")
+	r.HandleFunc("/organizer/{id}", controllers.GetOrganizerByID).Methods("GET")
+	r.HandleFunc("/organizer/{id}", controllers.UpdateOrganizer).Methods("PUT")
+	r.HandleFunc("/organizer/{id}", controllers.DeleteOrganizer).Methods("DELETE")
+
+	r.HandleFunc("/team", controllers.CreateTeam).Methods("POST", "OPTIONS")
+	r.HandleFunc("/getAllTeams", controllers.GetAllTeams).Methods("GET")
+	r.HandleFunc("/team/{id}", controllers.GetTeamByID).Methods("GET")
+	r.HandleFunc("/team/{id}", controllers.UpdateTeam).Methods("PUT")
+	r.HandleFunc("/team/{id}", controllers.DeleteTeam).Methods("DELETE")
 
 	fmt.Print("Server running on localhost:9000\n")
 	serverErr := http.ListenAndServe("localhost:9000", handlers.CORS(headers, methods, origins)(r))
